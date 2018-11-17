15b00f3a9aa9da2c7469c5c2474c01e37c01fdfa
JonathanDelm <jonathan.delmeiren.y1511@student.hogent.be>
2018-11-17 23:01:37 +0100

    Added first iteration of EventDetailPage

--- ProjectCityAppUWP/ViewModels/EventDetailPageViewModel.cs
+++ ProjectCityAppUWP/ViewModels/EventDetailPageViewModel.cs
@@ -1,19 +1,51 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SharedClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
 
 namespace ProjectCityAppUWP.ViewModels
 {
     public class EventDetailPageViewModel : ViewModelBase
     {
-        public string Test { get; set; }
+		public SharedEvent Event { get; set; }
+		public SharedCompany Company { get; set; }
+		public DelegateCommand<Guid> CmdGoToCompanyDetail { get; set; }
 
-        public EventDetailPageViewModel()
-        {
-            Test = "Event Detail";
-        }
-    }
+
+		public EventDetailPageViewModel()
+		{
+			CmdGoToCompanyDetail = new DelegateCommand<Guid>(GoToCompanyDetail);
+		}
+
+		public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+		{
+			GetEvent((Guid)parameter);
+			return base.OnNavigatedToAsync(parameter, mode, state);
+		}
+
+		public async void GetEvent(Guid eventId)
+		{
+			HttpClient client = new HttpClient();
+			string res = await client.GetStringAsync(new Uri("http://localhost:51070/api/event/" + eventId));
+			var tempEvent = JsonConvert.DeserializeObject<SharedEvent>(res);
+			Event = tempEvent;
+			RaisePropertyChanged("Event");
+
+			string res2 = await client.GetStringAsync(new Uri("http://localhost:51070/api/company/" + Event.FK_CompanyID));
+			var tempCompany = JsonConvert.DeserializeObject<SharedCompany>(res2);
+			Company = tempCompany;
+			RaisePropertyChanged("Company");
+		}
+
+		private void GoToCompanyDetail(Guid guid)
+		{
+			NavigationService.Navigate(typeof(Views.CompanyDetailPage), guid);
+		}
+	}
 }
