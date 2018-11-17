6279e8806ae977ca430bac9d2153ce3f3098fa69
JonathanDelm <jonathan.delmeiren.y1511@student.hogent.be>
2018-11-17 23:23:49 +0100

    Added first iteration of PromotionDetailPage

--- ProjectCityAppUWP/ViewModels/PromotionDetailPageViewModel.cs
+++ ProjectCityAppUWP/ViewModels/PromotionDetailPageViewModel.cs
@@ -1,19 +1,49 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SharedClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
 
 namespace ProjectCityAppUWP.ViewModels
 {
     public class PromotionDetailPageViewModel : ViewModelBase
     {
-        public string Test { get; set; }
+		public SharedPromotion Promotion { get; set; }
+		public SharedCompany Company { get; set; }
+		public DelegateCommand BtnDiscount { get; set; }
 
-        public PromotionDetailPageViewModel()
+		public PromotionDetailPageViewModel()
         {
-            Test = "Promotion Detail";
-        }
-    }
+			BtnDiscount = new DelegateCommand(OpenDiscountPdf);
+		}
+		private async void OpenDiscountPdf()
+		{
+			var success = await Windows.System.Launcher.LaunchUriAsync(new Uri("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"));
+		}
+
+		public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
+		{
+			GetPromotion((Guid)parameter);
+			return base.OnNavigatedToAsync(parameter, mode, state);
+		}
+
+		public async void GetPromotion(Guid promotionId)
+		{
+			HttpClient client = new HttpClient();
+			string res = await client.GetStringAsync(new Uri("http://localhost:51070/api/promotion/" + promotionId));
+			var tempPromotion = JsonConvert.DeserializeObject<SharedPromotion>(res);
+			Promotion = tempPromotion;
+			RaisePropertyChanged("Promotion");
+
+			string res2 = await client.GetStringAsync(new Uri("http://localhost:51070/api/company/" + Promotion.FK_CompanyID));
+			var tempCompany = JsonConvert.DeserializeObject<SharedCompany>(res2);
+			Company = tempCompany;
+			RaisePropertyChanged("Company");
+		}
+	}
 }
