4bf2e162d1274149d111ca7e5b0ece75d8506e45
Simeon Angerer <simeon.angerer@gmx.at>
2018-11-28 12:28:40 +0100

    Added Activity

--- ProjectCityAppUWP/ViewModels/PromotionDetailPageViewModel.cs
+++ ProjectCityAppUWP/ViewModels/PromotionDetailPageViewModel.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Template10.Mvvm;
+using Windows.ApplicationModel.UserActivities;
 using Windows.Media.SpeechSynthesis;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
@@ -37,9 +38,30 @@ namespace ProjectCityAppUWP.ViewModels
             audioPlayer.SetSource(ttsStream, "");
         }
 
+        UserActivitySession _currentActivity;
         private async void OpenDiscountPdf()
 		{
-			var success = await Windows.System.Launcher.LaunchUriAsync(new Uri("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"));
+            var success = await Windows.System.Launcher.LaunchUriAsync(new Uri("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf"));
+            await GenerateActivityAsync();
+        }
+
+        private async Task GenerateActivityAsync()
+        {
+            // Get the default UserActivityChannel and query it for our UserActivity. If the activity doesn't exist, one is created.
+            UserActivityChannel channel = UserActivityChannel.GetDefault();
+            UserActivity userActivity = await channel.GetOrCreateUserActivityAsync("MainPage");
+
+            // Populate required properties
+            userActivity.VisualElements.DisplayText = "ProjectCityAppUWP Activity";
+            userActivity.VisualElements.Description = "You downloaded a discount PDF!";
+            userActivity.ActivationUri = new Uri("my-app://MainPage?action=edit");
+
+            //Save
+            await userActivity.SaveAsync(); //save the new metadata
+
+            // Dispose of any current UserActivitySession, and create a new one.
+            _currentActivity?.Dispose();
+            _currentActivity = userActivity.CreateSession();
         }
 
         public override Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
